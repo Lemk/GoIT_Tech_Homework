1d1dd09dcdb6e3cd896635608ce93078871c8b22
Lemk <lemkin@ua.fm>
2016-03-30 01:37:54 +0300

    hw 3-4 fix

--- js/hw3-4/js/script.js
+++ js/hw3-4/js/script.js
@@ -1,54 +1,71 @@
-var tests = {
-    caption: 'Тест по программированию',
-    question: 'Вопрос №',
-    answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
-    captionBtn: 'Проверить мои результаты',
-    wrapper: document.createElement('div'),
-
-    initElements: function () {
-        this.wrapper.className = 'container';
-        var body = document.getElementsByTagName('body');
-        document.body.appendChild(this.wrapper);
-    },
-    setCaption: function () {
-        var name = document.createElement('h3');
-        name.innerHTML = '<p class="text-center">' + this.caption + '</p>';
-        this.wrapper.appendChild(name);
-    },
-    setQuestion: function () {
+var question = {
+    setQuestion: function (parent, question, answers) {
         var list;
         var listItem;
-        for (var i = 0; i < 3; i++) {
-            list = document.createElement('ul');
-            list.innerHTML = (i + 1) + '. ' + this.question + (i + 1);
-            for (var j = 0; j < this.answer.length; j++) {
-                listItem = document.createElement('li');
-                listItem.className = 'list-unstyled';
-                listItem.innerHTML = '<label><input class=check-' + i + j + "" + ' type="checkbox" value="">' +
-                    '<p>' + this.answer[j] + '</p></label>';
-                list.appendChild(listItem);
-            }
-            this.wrapper.appendChild(list);
-        }
 
-    },
-    setButton: function () {
-        var btnBox = document.createElement('div');
-        var button = document.createElement('button');
-        button.className = 'btn btn-default';
-        button.type = 'submit';
-        button.innerHTML = this.captionBtn;
-        btnBox.className = 'btnBox';
-        btnBox.appendChild(button);
-        this.wrapper.appendChild(btnBox);
-    },
-    show: function () {
-        this.initElements();
-        this.setCaption();
-        this.setQuestion();
-        this.setButton();
+        list = document.createElement('ul');
+        list.innerHTML = question;
+        for (var j = 0; j < answers.length; j++) {
+            listItem = document.createElement('li');
+            listItem.className = 'list-unstyled';
+            listItem.innerHTML = '<label><input type="checkbox" name=ch' + j + '>' + '<p>' + answers[j] + '</p></label>';
+            list.appendChild(listItem);
+        }
+        return parent.appendChild(list);
     }
+
 };
 
-tests.show();
+// заголовок
+var parent = initElements(document.body);
+setCaption(parent, 'Тест по программированию');
+parent = setForm(parent);
+// 1 блок
+var answers = ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'];
+question.setQuestion(parent, 'Вариант №1', answers);
+// 2 блок
+answers = ['Вариант ответа №4', 'Вариант ответа №5', 'Вариант ответа №6', 'Вариант ответа №7'];
+question.setQuestion(parent, 'Вариант №2', answers);
+// 3 блок
+answers = ['Вариант ответа №8', 'Вариант ответа №9', 'Вариант ответа №10'];
+question.setQuestion(parent, 'Вариант №3', answers);
+// кнопка отправки
+setButton(parent, 'Проверить мои результаты');
+
+
+function initElements(parent) {
+    wrapper = document.createElement('div');
+    wrapper.className = 'container';
+    return parent.appendChild(wrapper);
+};
+
+function setCaption(parent, caption) {
+    var name = document.createElement('h3');
+    name.innerHTML = '<p class="text-center">' + caption + '</p>';
+    return parent.appendChild(name);
+};
+
+function setForm(parent, caption) {
+    var name = document.createElement('form');
+    name.id = 'test';
+    return parent.appendChild(name);
+};
+
+
+function setButton(parent, caption) {
+    var btnBox = document.createElement('div');
+    var button = document.createElement('button');
+    button.className = 'btn btn-default';
+    button.type = 'submit';
+    button.innerHTML = caption;
+    btnBox.className = 'btnBox';
+    btnBox.appendChild(button);
+    return parent.appendChild(btnBox);
+};
+
+
+
+
+
+
 
