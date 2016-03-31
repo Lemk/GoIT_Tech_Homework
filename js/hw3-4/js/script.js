88cff8e5c3f7af1b65a58120d855f93aa8a071e1
Lemk <lemkin@ua.fm>
2016-03-31 23:14:24 +0300

    fix 3 hw3-4

--- js/hw3-4/js/script.js
+++ js/hw3-4/js/script.js
@@ -1,71 +1,64 @@
-var question = {
-    setQuestion: function (parent, question, answers) {
+var test = {
+    questionArray: [
+        {
+            question: 'Вариант №1',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        },
+        {
+            question: 'Вариант №2',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        },
+        {
+            question: 'Вариант №3',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        },
+    ],
+    caption: 'Тест по программированию',
+    btnCaption: 'Проверить мои результаты',
+    initElements: function (parent) {
+        var name = document.createElement('h3');
+        name.innerHTML = '<p class="text-center">' + this.caption + '</p>';
+        parent.appendChild(name);
+
+        var form = document.createElement('form');
+        form.id = 'test';
+        parent.appendChild(form);
+
+
+        var wrapper = document.createElement('div');
+        wrapper.className = 'container';
+        return form.appendChild(wrapper);
+    },
+    setQuestion: function (parent) {
         var list;
         var listItem;
-
-        list = document.createElement('ul');
-        list.innerHTML = question;
-        for (var j = 0; j < answers.length; j++) {
-            listItem = document.createElement('li');
-            listItem.className = 'list-unstyled';
-            listItem.innerHTML = '<label><input type="checkbox" name=ch' + j + '>' + '<p>' + answers[j] + '</p></label>';
-            list.appendChild(listItem);
+        for (var i = 0; i < this.questionArray.length; i++) {
+            list = document.createElement('ul');
+            list.innerHTML = this.questionArray[i].question;
+            for (var j = 0; j < this.questionArray[i].answers.length; j++) {
+                listItem = document.createElement('li');
+                listItem.className = 'list-unstyled';
+                listItem.innerHTML = '<label><input type="checkbox" name=ch' + i + j + '>' + '<p>' + this.questionArray[i].answers[j] + '</p></label>';
+                list.appendChild(listItem);
+            }
+            parent.appendChild(list);
         }
-        return parent.appendChild(list);
-    }
+    },
+    setButton: function (parent) {
+        var btnBox = document.createElement('div');
+        var button = document.createElement('button');
+        button.className = 'btn btn-default';
+        button.type = 'submit';
+        button.innerHTML = this.btnCaption;
+        btnBox.className = 'btnBox';
+        btnBox.appendChild(button);
+        parent.appendChild(btnBox);
+    },
+    show: function (parent) {
+        var group = this.initElements(parent);
+        this.setQuestion(group);
+        this.setButton(group);
+    },
+}
 
-};
-
-// заголовок
-var parent = initElements(document.body);
-setCaption(parent, 'Тест по программированию');
-parent = setForm(parent);
-// 1 блок
-var answers = ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'];
-question.setQuestion(parent, 'Вариант №1', answers);
-// 2 блок
-answers = ['Вариант ответа №4', 'Вариант ответа №5', 'Вариант ответа №6', 'Вариант ответа №7'];
-question.setQuestion(parent, 'Вариант №2', answers);
-// 3 блок
-answers = ['Вариант ответа №8', 'Вариант ответа №9', 'Вариант ответа №10'];
-question.setQuestion(parent, 'Вариант №3', answers);
-// кнопка отправки
-setButton(parent, 'Проверить мои результаты');
-
-
-function initElements(parent) {
-    wrapper = document.createElement('div');
-    wrapper.className = 'container';
-    return parent.appendChild(wrapper);
-};
-
-function setCaption(parent, caption) {
-    var name = document.createElement('h3');
-    name.innerHTML = '<p class="text-center">' + caption + '</p>';
-    return parent.appendChild(name);
-};
-
-function setForm(parent, caption) {
-    var name = document.createElement('form');
-    name.id = 'test';
-    return parent.appendChild(name);
-};
-
-
-function setButton(parent, caption) {
-    var btnBox = document.createElement('div');
-    var button = document.createElement('button');
-    button.className = 'btn btn-default';
-    button.type = 'submit';
-    button.innerHTML = caption;
-    btnBox.className = 'btnBox';
-    btnBox.appendChild(button);
-    return parent.appendChild(btnBox);
-};
-
-
-
-
-
-
-
+test.show(document.body);
